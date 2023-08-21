33cdd3660fe03fd7f47a275c0ab52823206e9aec
Hafiz2023 <adilamin374@gmail.com>
2023-08-21 10:12:20 +0500

    update

---
+++ src/app/mock.tsx
@@ -0,0 +1,81 @@
+export const contactData = [
+  {
+    id: 1,
+    image: "/contact.png",
+    name: "Sir Jahanzaib",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 2,
+    image: "/contact.png",
+    name: "Miss Hira",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 3,
+    image: "/contact.png",
+    name: "Mubeen Ameen",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 4,
+    image: "/contact.png",
+    name: "Waheed Siddique",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 5,
+    image: "/contact.png",
+    name: "Imran Jutt",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 6,
+    image: "/contact.png",
+    name: "Sir Zia Khan",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 7,
+    image: "/contact.png",
+    name: "Sir Adil Altaf",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 8,
+    image: "/contact.png",
+    name: "Sir Zeeshan Hanif",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 9,
+    image: "/contact.png",
+    name: "Sir Daniyal Nigori",
+    contact: "03xxxxxxxxx",
+  },
+ 
+  {
+    id: 10,
+    image: "/contact.png",
+    name: "Sir Kamran",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 11,
+    image: "/contact.png",
+    name: "Hanan",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 12,
+    image: "/contact.png",
+    name: "Hammad",
+    contact: "03xxxxxxxxx",
+  },
+  {
+    id: 13,
+    image: "/contact.png",
+    name: "Ahmed Bilal",
+    contact: "03xxxxxxxxx",
+  },
+];
